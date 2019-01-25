f006374e3b872bc71b23e51c768193eb7a09a8e0
Michal Topola <Misza_71@wp.pl>
2019-01-25 14:10:21 +0100

    Funkcja+referencje

---
+++ ConsoleApplication20/ConsoleApplication20.cpp
@@ -0,0 +1,35 @@
+﻿// ConsoleApplication20.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include "pch.h"
+#include <iostream>
+using namespace std;
+int load(int & number) {
+	cout << "Numer ktory dales mi do pracy to: " << number << endl;
+	number += 7;
+	cout << "A ja zrobilem z nia to: " << number << endl;
+	cout << "temat to refernecje zmienniajace wnetrze wiecj zaraz sie rozkrece " << endl;
+	return 0;
+}
+
+int main()
+{
+	int x;
+
+    std::cout << "Dawaj jakas liczbe\n"; 
+	cin >> x;
+	cout << "Ta liczba to: " << x<<endl;
+	load(x);
+	cout << "Teraz to: " << x << endl;
+}
+
+// Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
+// Debugowanie programu: F5 lub menu Debugowanie > Rozpocznij debugowanie
+
+// Porady dotyczące rozpoczynania pracy:
+//   1. Użyj okna Eksploratora rozwiązań, aby dodać pliki i zarządzać nimi
+//   2. Użyj okna programu Team Explorer, aby nawiązać połączenie z kontrolą źródła
+//   3. Użyj okna Dane wyjściowe, aby sprawdzić dane wyjściowe kompilacji i inne komunikaty
+//   4. Użyj okna Lista błędów, aby zobaczyć błędy
+//   5. Wybierz pozycję Projekt > Dodaj nowy element, aby utworzyć nowe pliki kodu, lub wybierz pozycję Projekt > Dodaj istniejący element, aby dodać istniejące pliku kodu do projektu
+//   6. Aby w przyszłości ponownie otworzyć ten projekt, przejdź do pozycji Plik > Otwórz > Projekt i wybierz plik sln
